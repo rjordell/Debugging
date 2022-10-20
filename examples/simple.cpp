5a2566de47b3e905db4a628b116498083bf06752
rjordell <rjordell@gmail.com>
2022-10-20 13:05:03 -0700

    Examples completed

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,6 +1,7 @@
 int main() {
     int *p = new int[10];
-    p[10] = 1;
+    p[9] = 1; // corrected from size - 1 = 10 - 1 = 9
 
+    delete[] p; // deallocate the memory from the new above
     return 0;
 }
